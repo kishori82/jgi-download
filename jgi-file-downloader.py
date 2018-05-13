cf07d8a0a7f9bfe505c5d41e5c2c335ac2b25c63
Kishori M Konwar <kishori82@gmail.com>
2018-05-13 10:38:15 -0400

    added a taxon to orgnaims map converter

--- jgi-file-downloader.py
+++ jgi-file-downloader.py
@@ -17,24 +17,44 @@ parser = optparse.OptionParser(usage=usage)
 parser.add_option("-o", "--organism", dest="organism",  default=None, help="organism name")
 parser.add_option("-l", "--login", dest="login", default=None, help="JGI login name")
 parser.add_option("-p", "--pass", dest="password", default=None, help="JGI password")
-parser.add_option("-f", "--file", dest="files",  default=[], action="append",  help="file names")
-parser.add_option("-L", "--list", dest="list",  action="store_true", default=False,  help="file names")
+parser.add_option("-f", "--file", dest="files",  default=[], action="append",  help="file to download")
+parser.add_option("-L", "--list", dest="list",  action="store_true", default=False,  help="list file names")
+parser.add_option("-m", "--map", dest="taxon_to_organism",  default=None,  help="taxon to organism map file")
+parser.add_option("-t", "--taxon", dest="taxon",  default=None,  help="taxon name")
 
 def main(argv):
    global parser
-   organism = argv[1]
-   files = argv[2:]
    opt, args = parser.parse_args(argv)
 
    if opt.login:
       loginToJGI(opt)
 
+   if opt.taxon_to_organism:
+      taxon_to_organism=read_taxon_to_organism(opt.taxon_to_organism)
+      if opt.taxon and  opt.organism == None:
+         opt.organism = taxon_to_organism[opt.taxon]
+
+      if opt.taxon==None and  opt.organism:
+         opt.taxon = taxon_to_organism[opt.organism]
+
+
+         
    if opt.files and opt.organism:
       downloadFiles(opt)
 
    if opt.list and opt.organism:
       listFiles(opt)
 
+def read_taxon_to_organism(taxon_to_organism_file):
+   taxon_to_organism_map={}
+   with open(taxon_to_organism_file, 'r') as fin:
+      for line in fin:
+         fields = [ x.strip() for x in line.strip().split('\t') ]
+         if len(fields)==2:
+            taxon_to_organism_map[fields[0]] = fields[1]
+            taxon_to_organism_map[fields[1]] = fields[0]
+      
+   return taxon_to_organism_map
 
 
 def loginToJGI(opts):
@@ -58,7 +78,7 @@ def listFiles(opt):
 
    i = 1
    print
-   print  "FILES:"
+   print  "FILES: ", opt.taxon, opt.organism
    with open(organism + ".xml", 'r') as fin:
       for line in fin:
          res = refiles.search(line)
